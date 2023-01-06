37472dc803243784f9dac3d507662d9fae4824f9
sinkolinko <leoliker@gmail.com>
2023-01-06 15:08:53 -0500

    code written, setinterval bugs are present

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 "use strict";
 
-setInterval(playNotes, 3500);
-
-const note = document.getElementById("note-1");
+const note = document.getElementById("note");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
 
 const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
-function playNotes() {
-  const random = Math.floor(Math.random() * notes.length);
-  console.log(notes[random]);
-}
+// let myTimer = setInterval(playNotes, 1000);
+// var myTimer;
 
-function startNotes() {
-  document.getElementById("start-1").addEventListener("click", function () {
-    note.textContent = notes[random];
-  });
-}
+// function playNotes() {
+//   const random = Math.floor(Math.random() * notes.length);
+//   note.textContent = notes[random];
+//   console.log(notes[random]);
+//   if (start.classList.contains("hidden")) {
+//     document.getElementById("start").addEventListener("click", function () {
+//       start.classList.remove("hidden");
+//       stop.classList.add("hidden");
+//       console.log(notes[random]);
+//       const myTimer = setInterval(playNotes, 1000);
+//     });
+//   } else if (stop.classList.contains("hidden")) {
+//     document.getElementById("stop").addEventListener("click", function () {
+//       stop.classList.remove("hidden");
+//       start.classList.add("hidden");
+//       clearInterval(myTimer);
+//     });
+//   }
+// }
 
-startNotes();
+document.getElementById("start").addEventListener("click", function () {
+  // if (this.classList.contains("start")) {
+  const myTimer = setInterval(() => {
+    if (this.classList.contains("start")) {
+      const random = Math.floor(Math.random() * notes.length);
+      note.textContent = notes[random];
+      console.log(notes[random]);
+      this.classList.remove("start");
+      this.classList.add("stop");
+      this.textContent = "stop";
+    } else {
+      this.classList.remove("stop");
+      this.classList.add("start");
+      this.textContent = "play";
+      clearInterval(myTimer);
+    }
+  }, 1000);
 
-playNotes();
+  // } else {
+});
