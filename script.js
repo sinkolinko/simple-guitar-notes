1f3767df884d46265afcb53c3655e609d6bbb6e1
sinkolinko <leoliker@gmail.com>
2023-01-09 17:40:00 -0500

    Added change of speed to the notes with a limit

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-'use strict';
+"use strict";
 
-const note = document.getElementById('note');
-const start = document.getElementById('start');
+const note = document.getElementById("note");
+const start = document.getElementById("start");
 
-const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
 let noteCounter = null;
+let noteSpeed = 1000;
 
-start.addEventListener('click', function () {
-  if (start.classList.contains('start')) {
-    noteCounter = setInterval(startNotes, 1000);
-    start.classList.remove('start');
-    start.classList.add('stop');
-    start.textContent = 'stop';
+start.addEventListener("click", function () {
+  if (start.classList.contains("start")) {
+    noteCounter = setInterval(startNotes, noteSpeed);
+    start.classList.remove("start");
+    start.classList.add("stop");
+    start.textContent = "stop";
   } else {
     clearInterval(noteCounter);
-    start.classList.remove('stop');
-    start.classList.add('start');
-    start.textContent = 'play';
+    start.classList.remove("stop");
+    start.classList.add("start");
+    start.textContent = "play";
   }
+});
+
+document.getElementById("faster").addEventListener("click", function () {
+  if (noteSpeed <= 500) {
+  } else {
+    clearInterval(noteCounter);
+    noteSpeed = noteSpeed - 500;
+    noteCounter = setInterval(startNotes, noteSpeed);
+  }
+});
+
+document.getElementById("slower").addEventListener("click", function () {
+  clearInterval(noteCounter);
+  noteSpeed = noteSpeed + 500;
+  noteCounter = setInterval(startNotes, noteSpeed);
 });
 
 function startNotes() {
