bdc70bd94873daa4d4a60d551747e5396de6ddd6
sinkolinko <leoliker@gmail.com>
2023-01-26 13:04:54 -0500

    implemented a limit warning for note speed

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-'use strict';
+"use strict";
 
-const note = document.getElementById('note');
-const start = document.getElementById('start');
+const note = document.getElementById("note");
+const start = document.getElementById("start");
+const slower = document.getElementById("slower");
+const faster = document.getElementById("faster");
 
-const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
 let noteCounter = null;
 let noteSpeed = 1000;
 
-start.addEventListener('click', function () {
-  if (start.classList.contains('start')) {
+start.addEventListener("click", function () {
+  if (start.classList.contains("start")) {
     noteCounter = setInterval(startNotes, noteSpeed);
-    start.classList.remove('start');
-    start.classList.add('stop');
-    start.innerHTML = 'stop';
+    start.classList.remove("start");
+    start.classList.add("stop");
+    start.innerHTML = "stop";
   } else {
     clearInterval(noteCounter);
-    start.classList.remove('stop');
-    start.classList.add('start');
-    start.textContent = 'play';
+    start.classList.remove("stop");
+    start.classList.add("start");
+    start.textContent = "play";
   }
 });
 
-document.getElementById('faster').addEventListener('click', function () {
-  if (noteSpeed <= 500) {
-  } else {
-    clearInterval(noteCounter);
-    noteSpeed = noteSpeed - 500;
-    noteCounter = setInterval(startNotes, noteSpeed);
-  }
-});
+document
+  .getElementById("faster")
+  .addEventListener("click", function fasterNotes() {
+    if (noteSpeed <= 500) {
+      faster.textContent = "X";
+    } else {
+      clearInterval(noteCounter);
+      noteSpeed = noteSpeed - 500;
+      noteCounter = setInterval(startNotes, noteSpeed);
+      faster.textContent = "+";
+      slower.textContent = "-";
+    }
+  });
 
-document.getElementById('slower').addEventListener('click', function () {
-  clearInterval(noteCounter);
-  noteSpeed = noteSpeed + 500;
-  noteCounter = setInterval(startNotes, noteSpeed);
-});
+document
+  .getElementById("slower")
+  .addEventListener("click", function slowerNotes() {
+    if (noteSpeed >= 1500) {
+      slower.textContent = "X";
+    } else {
+      clearInterval(noteCounter);
+      noteSpeed = noteSpeed + 500;
+      noteCounter = setInterval(startNotes, noteSpeed);
+      faster.textContent = "+";
+      slower.textContent = "-";
+    }
+  });
 
 function startNotes() {
   const random = Math.floor(Math.random() * notes.length);
